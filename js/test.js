0dba46c69ea04011c956b9624edaf8af5645f8aa
Andrew Butenko <sedkol88@gmail.com>
2023-10-20 23:40:11 +0300

    test lessons

--- js/test.js
+++ js/test.js
@@ -1,7 +1,44 @@
-let arr = []
-for (let i = 1; i <= 500; i += 1) {
-    if (i % 2 === 1 && i % 3 === 1 && i % 4 === 1 && i % 5 === 1 && i % 6 === 1 && i % 7 === 0) {
-        arr.push(i)
+// const obj = {
+//     array: [83, 44, 6, 79],
+//     dividedByTwo: function () {
+//         const newArray = [];
+//         this.array.forEach((arr) => {
+//             if (arr % 2 === 1) {
+//                 newArray.push(arr);
+//             }
+//         })
+//         console.log(newArray)
+//     }
+// }
+// // console.log(obj.dividedByTwo())
+// obj.dividedByTwo()
+
+const form = document.querySelector('.js-form')
+form.addEventListener('submit', handleSubmit)
+function handleSubmit(event) {
+    event.preventDefault()
+    const{input, comment} = event.currentTarget.elements
+    const feedback = {
+      input: input.value,
+      comment: comment.value
     }
+  console.log(feedback)
+  // event.currentTarget.reset()
+  event.currentTarget.elements.comment.value = ''
 }
-console.log(arr)
+
+
+// const obj = {
+//     array: [83, 44, 6, 79],
+//     dividedByTwo: function() {
+//         const newArray = [];
+//         this.array.forEach(function(arr) {
+//             if (arr % 2 === 0) {
+//                 newArray.push(arr);
+//             }
+//         });
+//         return newArray;
+//     }
+// };
+
+// console.log(obj.dividedByTwo())
