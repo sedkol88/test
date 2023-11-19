d714fe21b001dec2058bbf9e7e3ef89f1bfec701
Andrew Butenko <sedkol88@gmail.com>
2023-11-19 21:41:44 +0200

    111

--- js/test.js
+++ js/test.js
@@ -1,44 +1,11 @@
-// const obj = {
-//     array: [83, 44, 6, 79],
-//     dividedByTwo: function () {
-//         const newArray = [];
-//         this.array.forEach((arr) => {
-//             if (arr % 2 === 1) {
-//                 newArray.push(arr);
-//             }
-//         })
-//         console.log(newArray)
-//     }
-// }
-// // console.log(obj.dividedByTwo())
-// obj.dividedByTwo()
+const makeGreeting = guestName => {
+  if (guestName === "" || guestName === undefined) {
+    return Promise.reject("Guest name must not be empty");
+  }
 
-const form = document.querySelector('.js-form')
-form.addEventListener('submit', handleSubmit)
-function handleSubmit(event) {
-    event.preventDefault()
-    const{input, comment} = event.currentTarget.elements
-    const feedback = {
-      input: input.value,
-      comment: comment.value
-    }
-  console.log(feedback)
-  // event.currentTarget.reset()
-  event.currentTarget.elements.comment.value = ''
-}
+  return Promise.resolve(`Welcome ${guestName}`);
+};
 
-
-// const obj = {
-//     array: [83, 44, 6, 79],
-//     dividedByTwo: function() {
-//         const newArray = [];
-//         this.array.forEach(function(arr) {
-//             if (arr % 2 === 0) {
-//                 newArray.push(arr);
-//             }
-//         });
-//         return newArray;
-//     }
-// };
-
-// console.log(obj.dividedByTwo())
+makeGreeting("")
+  .then(greeting => console.log(greeting))
+  .catch(error => console.error(error));
