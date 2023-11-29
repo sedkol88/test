012a67677a6c94120322dc3b2ee7045ae6c4e768
Andrew Butenko <sedkol88@gmail.com>
2023-11-29 22:59:40 +0200

    1111

--- js/test.js
+++ js/test.js
@@ -1,81 +1,104 @@
 const refs = {
-  movieList: document.querySelector('.js-movie-list'),
-  loadMoreBtn: document.querySelector('.js-load-more'),
+  form: document.querySelector('.js-search'),
+  formContainer: document.querySelector('.js-form-container'),
+  addBtn: document.querySelector('.js-add'),
+  list: document.querySelector('.js-list'),
+};
+
+refs.addBtn.addEventListener('click', handleAddField);
+refs.form.addEventListener('submit', handleSubmitCountries);
+
+function handleAddField() {
+  refs.formContainer.insertAdjacentHTML(
+    'beforeend',
+    '<input type="text" name="country" />'
+  );
 }
 
-const classes = {
-  loadMoreHidden: 'load-more-hidden',
-}
+async function handleSubmitCountries(event) {
+  //выключение дефолтной перезагрузки страницы
+  event.preventDefault();
 
-let page = 499;
+  //получение данных из формы
+  const formData = new FormData(event.currentTarget);
+  //валидация данных
+  const countries = formData
+    .getAll('country')
+    .map(value => value.trim())
+    .filter(value => value);
+  console.log(countries);
 
-serviceMovies()
-  .then((data) => {
-    console.log(data);
-    refs.movieList.innerHTML = createMarkup(data.results);
-
-    if (data.page < data.total_pages && data.page < 500) {
-      refs.loadMoreBtn.classList.remove(classes.loadMoreHidden);
-      refs.loadMoreBtn.addEventListener('click', handleLoadMore)
-    }
-
-  })
-  .catch((err) => {
+  try {
+    //последовательные запросы столиц и погоды в этих столицах
+    const capitals = await serviceCountries(countries);
+    const weather = await serviceWeather(capitals);
+    console.log(capitals);
+    console.log(weather);
+    refs.list.innerHTML = createMarcup(weather);
+  } catch (err) {
     console.log(err);
-  });
-
-
-
-function serviceMovies (page = 1) {
-  const BASE_URL = 'https://api.themoviedb.org/3';
-  const END_POINT = 'trending/movie/week';
-  const API_KEY = '92a9a9e3708a3e9451b7037d5906879a';
-
-  const params = new URLSearchParams({
-    api_key: API_KEY,
-    page,
-  });
-
-  return fetch(`${BASE_URL}/${END_POINT}?${params}`)
-    .then((resp) => {
-        if (!resp.ok) {
-          throw new Error(resp.statusText)
-      }
-      return resp.json()
-    })
+  } finally {
+    event.target.reset();
+  }
 }
 
-function createMarkup(arr) {
-  return arr.map(
-    ({ poster_path, title, vote_average, release_date }) => `<li class='movie-card'>
-    <img src='https://image.tmdb.org/t/p/w500/${poster_path}' alt='${title}'>
-    <div class='movie-info'>
-    <h2>${title}</h2>
-    <p>vote_average: ${vote_average}</p>
-    <p>release_date: ${release_date}</p>
-    </div>
-    </li>`
-  ).join('')
+//функция запроса столиц по названиям стран
+async function serviceCountries(countries) {
+  //перебирает массив стран и делает параллельный запрос по всем странам
+  const response = countries.map(async country => {
+    const { data } = await axios.get(
+      `https://restcountries.com/v3.1/name/${country}`
+    );
+    return data;
+  });
+  //делает из массива промисов массив ответов в состоянии settled
+  const results = await Promise.allSettled(response);
+  console.log(results);
+  //фильтрует массив и оставляет только элементы в состоянии fulfilled
+  return (
+    results
+      .filter(({ status }) => status === 'fulfilled')
+      //перебирает массив и выводит в новом массиве значения capital
+      .map(({ value }) => value[0].capital[0])
+  );
 }
 
-function handleLoadMore(event) {
-  page += 1;
-  refs.loadMoreBtn.disabled = true;
-
-  serviceMovies(page)
-  .then((data) => {
-    console.log(data);
-    refs.movieList.insertAdjacentHTML('beforeend', createMarkup(data.results));
-
-    if (data.page >= 500) {
-      refs.loadMoreBtn.classList.add(classes.loadMoreHidden);
-      refs.loadMoreBtn.removeEventListener('click', handleLoadMore)
-      return
-    }
-
-    refs.loadMoreBtn.disabled = false;
-  })
-  .catch((err) => {
-    console.log(err);
+async function serviceWeather(capitals) {
+  BASE_URL = 'http://api.weatherapi.com/v1';
+  ENDPOINT = 'forecast.json';
+  API_KEY = '66f9e81543404d02beb160521230808';
+  //перебирает массив столиц и делает параллельный запрос по всем столицам
+  const response = capitals.map(async capital => {
+    const { data } = await axios.get(`
+    ${BASE_URL}/${ENDPOINT}?key=${API_KEY}&q=${capital}&lang=uk
+    `);
+    return data;
   });
+  //делает из массива промисов массив объектов в состоянии settled
+  const results = await Promise.allSettled(response);
+  console.log(results);
+  //фильтрует массив и оставляет массив объектов в состоянии fulfilled
+  return results
+    .filter(({ status }) => status === 'fulfilled')
+    .map(({ value: { current, location } }) => {
+      const {
+        temp_c,
+        condition: { text, icon },
+      } = current;
+      const { country, name } = location;
+      return { temp_c, text, icon, country, name };
+    });
+}
+
+function createMarcup(arr) {
+  return arr
+    .map(
+      ({ temp_c, text, icon, country, name }) => `<li class="weather-card">
+      <img src="${icon}" alt="${text}" class="weather-icon">
+      <h2>${name}, ${country}</h2>
+      <h3 class="weather-text">${text}</h3>
+      <h3 class="temperature">${temp_c}</h3>
+  </li>`
+    )
+    .join('');
 }
